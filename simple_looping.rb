00fd7b636835b0fe6e55b06fdb10fb174b74c30f
Gina Fedder <ginafedder@yahoo.com>
2018-09-07 21:10:22 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ def loop_iterator(number_of_times)
   loop do
     counter = counter + 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if counter break if number_of_times
+    if counter >= number_of_times
+      break
   end
 end
 
